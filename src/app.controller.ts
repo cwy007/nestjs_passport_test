02594f6a16fb59fd685b1d564d08f5d634b2fa35
chanweiyan <chanweiyan007@gmail.com>
2026-05-08 12:21:34 +0800

    fix: test login by username and password

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AuthGuard } from '@nestjs/passport';
+import { type Request } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Req() req: Request) {
+    console.log(req.user);
+    return req.user;
   }
 }
